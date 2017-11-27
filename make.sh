099f5050f10976f5a2197ab41a85ccf3ee1fa87a
SuperDuperHacker <nagakamira@gmail.com>
2017-11-27 20:56:29 +0300

    host.conf

--- make.sh
+++ make.sh
@@ -87,7 +87,7 @@ prepare_filesystem() {
 	touch ${pkgdir}/var/log/lastlog
 	chmod -v 664 ${pkgdir}/var/log/lastlog
 
-	for f in fstab group hosts passwd profile securetty shells mdev.conf inittab hostname issue resolv.conf; do
+	for f in fstab group hosts passwd profile securetty shells mdev.conf inittab hostname issue resolv.conf host.conf; do
 		install -m644 ${stuffdir}/${f} etc/
 	done
 
