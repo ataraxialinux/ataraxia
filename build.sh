c755804e77ad56121c23283174c6915bbc0ca005
Your Name <nagakamira@gmail.com>
2017-12-18 15:12:22 +0300

    new system(toolchain will be added later)

--- build.sh
+++ build.sh
@@ -36,14 +36,13 @@ do_build_after_toolchain() {
 	export READELF="$TARGET-readelf"
 	export STRIP="$TARGET-strip"
 	export SIZE="$TARGET-size"
-
 }
 
 do_build_cross_config() {
 	case $XARCH in
 		x86_64)
 			export HOST=$(echo ${MACHTYPE} | sed -e 's/-[^-]*/-cross/')
-			export TARGET="x86_64-pc-linux-musl"
+			export TARGET="x86_64-janus-linux-musl"
 			export KARCH="x86_64"
 			export LIBSUFFIX="64"
 			export MULTILIB="--enable-multilib --with-multilib-list=m64"
@@ -51,7 +50,7 @@ do_build_cross_config() {
 			;;
 		i386)
 			export HOST=$(echo ${MACHTYPE} | sed -e 's/-[^-]*/-cross/')
-			export TARGET="i686-pc-linux-musl"
+			export TARGET="i686-janus-linux-musl"
 			export KARCH="i386"
 			export LIBSUFFIX=
 			export MULTILIB="--enable-multilib --with-multilib-list=m32"
@@ -59,7 +58,7 @@ do_build_cross_config() {
 			;;
 		aarch64)
 			export HOST=$(echo ${MACHTYPE} | sed -e 's/-[^-]*/-cross/')
-			export TARGET="aarch64-pc-linux-musl"
+			export TARGET="aarch64-janus-linux-musl"
 			export KARCH="arm64"
 			export LIBSUFFIX=
 			export MULTILIB="--disable-multilib --with-multilib-list="
@@ -67,7 +66,7 @@ do_build_cross_config() {
 			;;
 		arm)
 			export HOST=$(echo ${MACHTYPE} | sed -e 's/-[^-]*/-cross/')
-			export TARGET="arm-pc-linux-musleabihf"
+			export TARGET="arm-janus-linux-musleabihf"
 			export KARCH="arm"
 			export LIBSUFFIX=
 			export MULTILIB="--disable-multilib --with-multilib-list="
@@ -311,75 +310,343 @@ do_build_basic_system() {
 	make DESTDIR=$ROOTFS install
 
 	cd $SRC
-	wget http://ftp.gnu.org/gnu/binutils/binutils-2.29.1.tar.bz2
-	tar -xf binutils-2.29.1.tar.bz2
-	cd binutils-2.29.1
-	mkdir build
-	cd build
-	../configure \
+	wget ftp://ftp.astron.com/pub/file/file-5.32.tar.gz
+	tar -xf file-5.32.tar.gz
+	cd file-5.32
+	./configure \
 		$CONFIGURE \
 		$LINKING \
-		--with-build-sysroot=$ROOTFS \
-		--with-system-zlib \
-		--enable-deterministic-archives \
-		--enable-ld=default \
-		--enable-gold \
-		--enable-plugins \
-		--enable-threads \
-		--disable-multilib \
-		--disable-nls \
-		--disable-werror \
-		--disable-compressed-debug-sections \
-		--host=$TARGET --target=$TARGET --build=$TARGET
+		--host=$TARGET
 	make -j$JOBS
 	make DESTDIR=$ROOTFS install
-	rm -rf $ROOTFS/usr/lib/*.la
 
 	cd $SRC
-	wget http://ftp.gnu.org/gnu/gcc/gcc-7.2.0/gcc-7.2.0.tar.xz
-	tar -xf gcc-7.2.0.tar.xz
-	cd gcc-7.2.0
-	sed -i 's@\./fixinc\.sh@-c true@' gcc/Makefile.in
-	sed -i 's@pthread_yield();@sched_yield();@' libcilkrts/runtime/os-unix.c
-	export gcc_cv_prog_makeinfo_modern=no
-	export libat_cv_have_ifunc=no
-	mkdir build
-	cd build
-	../configure \
+	wget http://ftp.barfooze.de/pub/sabotage/tarballs/netbsd-curses-0.2.1.tar.xz
+	tar -xf netbsd-curses-0.2.1.tar.xz
+	cd netbsd-curses-0.2.1
+    cat << EOF > config.mak
+CFLAGS=-fPIC
+PREFIX=/usr
+DESTDIR=$ROOTFS
+EOF
+	make -j$JOBS
+	make install
+
+	cd $SRC
+	wget http://ftp.gnu.org/gnu/readline/readline-7.0.tar.gz
+	tar -xf readline-7.0.tar.gz
+	cd readline-7.0
+	./configure \
 		$CONFIGURE \
 		$LINKING \
-		--with-build-sysroot=$ROOTFS \
-		--with-system-zlib \
-		--enable-fully-dynamic-string \
-		--enable-checking=release \
-		--enable-languages=c,c++ \
-		--enable-c99 \
-		--enable-libstdcxx-time \
-		--enable-long-long \
-		--enable-tls \
-		--enable-lto \
-		--enable-linker-build-id \
-		--enable-threads=posix \
-		--enable-clocale=generic \
-		--enable-__cxa_atexit \
-		--disable-bootstrap \
-		--disable-fixed-point \
-		--disable-libitm \
-		--disable-libssp \
-		--disable-libmpx \
-		--disable-libmudflap \
-		--disable-libsanitizer \
-		--disable-libstdcxx-pch \
-		--disable-gnu-indirect-function \
-		--disable-libunwind-exceptions \		
-		--disable-multilib \
-		--disable-nls \
-		--disable-symvers \
-		--disable-werror \
-		--host=$TARGET --target=$TARGET --build=$TARGET
+		--host=$TARGET
 	make -j$JOBS
 	make DESTDIR=$ROOTFS install
-	rm -rf $ROOTFS/usr/lib/*.la
+
+	cd $SRC
+	wget http://ftp.gnu.org/gnu/bash/bash-4.4.12.tar.gz
+	tar -xf bash-4.4.12.tar.gz
+	cd bash-4.4.12
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--without-bash-malloc \
+		--with-installed-readline \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://download.savannah.gnu.org/releases/attr/attr-2.4.47.src.tar.gz
+	tar -xf attr-2.4.47.src.tar.gz
+	cd attr-2.4.47
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--disable-gettext \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install install-lib install-dev
+
+	cd $SRC
+	wget http://download.savannah.gnu.org/releases/acl/acl-2.2.52.src.tar.gz
+	tar -xf acl-2.2.52.src.tar.gz
+	cd acl-2.2.52
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--disable-gettext \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install install-lib install-dev
+
+	cd $SRC
+	wget http://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.xz
+	tar -xf m4-1.4.18.tar.xz
+	cd m4-1.4.18
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://ftp.gnu.org/gnu/sed/sed-4.4.tar.xz
+	tar -xf sed-4.4.tar.xz
+	cd sed-4.4
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--disable-i18n \
+		--disable-nls \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://ftp.gnu.org/gnu/gawk/gawk-4.2.0.tar.xz
+	tar -xf gawk-4.2.0.tar.xz
+	cd gawk-4.2.0
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--disable-nls \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.43.7/e2fsprogs-1.43.7.tar.gz
+	tar -xf e2fsprogs-1.43.7.tar.gz
+	cd e2fsprogs-1.43.7
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--enable-elf-shlibs \
+		--enable-symlink-install \
+		--disable-fsck \
+		--disable-uuidd \
+		--disable-libuuid \
+		--disable-libblkid \
+		--disable-tls \
+		--disable-nls \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://tukaani.org/xz/xz-5.2.3.tar.xz
+	tar -xf xz-5.2.3.tar.xz
+	cd xz-5.2.3
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--disable-rpath \
+		--disable-werror \
+		--disable-nls \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget https://www.kernel.org/pub/linux/utils/kernel/kmod/kmod-24.tar.xz
+	tar -xf kmod-24.tar.xz
+	cd kmod-24
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--with-rootlibdir=/usr/lib \
+		--with-xz \
+		--with-zlib \
+		--disable-manpages \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://dev.gentoo.org/~blueness/eudev/eudev-3.2.5.tar.gz
+	tar -xf eudev-3.2.5.tar.gz
+	cd eudev-3.2.5
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--disable-manpages \
+		--disable-selinux \
+		--enable-kmod \
+		--enable-blkid \
+		--enable-rule-generator \
+		--enable-hwdb \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://ftp.gnu.org/gnu/diffutils/diffutils-3.6.tar.xz
+	tar -xf diffutils-3.6.tar.xz
+	cd diffutils-3.6
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--disable-nls \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget https://sourceforge.net/projects/psmisc/files/psmisc/psmisc-23.1.tar.xz
+	tar -xf psmisc-23.1.tar.xz
+	cd psmisc-23.1
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://www.greenwoodsoftware.com/less/less-487.tar.gz
+	tar -xf less-487.tar.gz
+	cd less-487
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget ftp://ftp.vim.org/pub/vim/unix/vim-8.0.586.tar.bz2
+	tar -xf vim-8.0.586.tar.bz2
+	cd vim-8.0.586
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--with-vim-name=vim \
+		--without-x \
+		--enable-multibyte \
+		--enable-cscope \
+		--disable-gui \
+		--disable-gpm \
+		--disable-nls \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget https://www.sudo.ws/dist/sudo-1.8.21p2.tar.gz
+	tar -xf sudo-1.8.21p2.tar.gz
+	cd sudo-1.8.21p2
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--with-env-editor \
+		--without-pam \
+		--without-skey \
+		--enable-pie \
+		--disable-nls \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://ftp.gnu.org/gnu/tar/tar-1.29.tar.xz
+	tar -xf tar-1.29.tar.xz
+	cd tar-1.29
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.6.3.tar.gz
+	tar -xf libressl-2.6.3.tar.gz
+	cd libressl-2.6.3
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget https://fastly.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.6p1.tar.gz
+	tar -xf openssh-7.6p1.tar.gz
+	cd openssh-7.6p1
+	./configure \
+		$LINKING \
+		--prefix=/usr \
+		--sysconfdir=/etc/ssh \
+		--with-privsep-path=/var/lib/sshd \
+		--with-md5-passwords \
+		--with-ssl-engine \
+		--without-pie \
+		--without-pam \
+		--without-selinux \
+		--disable-lastlog \
+		--disable-utmp \
+		--disable-utmpx \
+		--disable-wtmp \
+		--disable-wtmpx \
+		--disable-strip \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget https://curl.haxx.se/download/curl-7.57.0.tar.xz
+	tar -xf curl-7.57.0.tar.xz
+	cd curl-7.57.0
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--without-librtmp \
+		--with-ssl \
+		--enable-ipv6 \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget https://invisible-mirror.net/archives/lynx/tarballs/lynx2.8.8rel.2.tar.Z
+	tar -xf lynx2.8.8rel.2.tar.Z
+	cd lynx2-8-8
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--with-ssl \
+		--enable-ipv6 \
+		--disable-nls \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://prdownloads.sourceforge.net/expat/expat-2.2.5.tar.bz2
+	tar -xf expat-2.2.5.tar.bz2
+	cd expat-2.2.5
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
+
+	cd $SRC
+	wget http://www.libarchive.org/downloads/libarchive-3.3.2.tar.gz
+	tar -xf libarchive-3.3.2.tar.gz
+	cd libarchive-3.3.2
+	./configure \
+		$CONFIGURE \
+		$LINKING \
+		--without-openssl \
+		--without-xml2 \
+		--without-expat \
+		--host=$TARGET
+	make -j$JOBS
+	make DESTDIR=$ROOTFS install
 }
 
 do_build_post_build() {
