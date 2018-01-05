f03cb7b9a30f0060ada954a1aa650f4191dd1630
nee-san <nagisurame@gmail.com>
2018-01-05 05:20:21 -0800

    fixing linking with gcc

--- build.sh
+++ build.sh
@@ -27,11 +27,12 @@ prepare_build() {
 	export MAKEOPTS="-j$(expr $(nproc) + 1)"
 
 	export PATH="$TOOLS/bin:$PATH"
+	export LD_LIBRARY_PATH="$TOOLS/lib"
 
 	export LC_ALL=POSIX
 	export LANG=POSIX
 
-	export CONFIGURE="--prefix=/usr --libdir=/usr/lib --libexecdir=/usr/libexec --bindir=/usr/bin --sbindir=/usr/bin --sysconfdir=/etc --localstatedir=/var --disable-static"
+	export CONFIGURE="--prefix=/usr --libdir=/usr/lib --libexecdir=/usr/libexec --bindir=/usr/bin --sbindir=/usr/bin --sysconfdir=/etc --localstatedir=/var --enable-shared --disable-static"
 
 	export XCFLAGS="-g0 -Os -s -pipe -fno-stack-protector -fomit-frame-pointer -U_FORTIFY_SOURCE"
 	export XCXXFLAGS="$XCFLAGS"
@@ -41,20 +42,18 @@ prepare_build() {
 prepare_cross() {
 	case $BARCH in
 		x86_64)
-			export XTARGET="x86_64-linux-musl"
+			export XHOST="$(echo ${MACHTYPE} | sed -e 's/-[^-]*/-cross/')"
+			export XTARGET="x86_64-pc-linux-musl"
 			export XKARCH="x86_64"
+			export MULTILIB="--enable-multilib --with-multilib-list=m64"
+			export GCCOPTS="--with-arch=x86-64 --with-tune=generic --enable-long-long"
 			;;
 		i486)
-			export XTARGET="i486-linux-musl"
+			export XHOST="$(echo ${MACHTYPE} | sed -e 's/-[^-]*/-cross/')"
+			export XTARGET="i486-pc-linux-musl"
 			export XKARCH="i386"
-			;;
-		arm64)
-			export XTARGET="aarch64-linux-musl"
-			export XKARCH="arm64"
-			;;
-		arm)
-			export XTARGET="arm-linux-musleabihf"
-			export XKARCH="arm"
+			export MULTILIB="--enable-multilib --with-multilib-list=m32"
+			export GCCOPTS="--with-arch=i486 --with-tune=generic"
 			;;
 		*)
 			echo "BARCH variable isn't set..."
@@ -63,10 +62,13 @@ prepare_cross() {
 }
 
 build_prepare() {
-	unset CFLAGS CXXFLAGS LDFLAGS
+	rm -rf $SOURCES/*
+	unset CFLAGS CXXFLAGS LDFLAGS LD_LIBRARY_PATH LIBS
 	export CFLAGS="$XCFLAGS"
 	export CXXFLAGS="$XCXXFLAGS"
 	export LDFLAGS="$XLDFLAGS -Wl,-rpath,$ROOTFS/usr/lib"
+	export LD_LIBRARY_PATH="$ROOTFS/usr/lib"
+	export LIBS="-L$ROOTFS/usr/lib" \
 	export CC="$XTARGET-gcc --sysroot=$ROOTFS"
 	export CXX="$XTARGET-g++ --sysroot=$ROOTFS"
 	export LD="$XTARGET-ld --sysroot=$ROOTFS"
@@ -80,10 +82,154 @@ build_prepare() {
 	export SIZE="$XTARGET-size"
 }
 
-get_toolchain() {
+prepare_toolchain() {
+	cd $TOOLS
+	mkdir bin lib
+
+	ln -sf bin sbin
+
+	case $BARCH in
+		x86_64)
+			ln -sf lib lib64
+			;;
+	esac
+
+	ln -sf . usr
+}
+
+cook_toolchain() {
 	cd $SOURCES
-	wget -c https://github.com/jprjr/musl-cross-make-static/releases/download/4/gcc-7.2.0-$XTARGET.tar.gz
-	tar -xf gcc-7.2.0-$XTARGET.tar.gz -C $TOOLS
+	wget -c http://ftp.gnu.org/gnu/binutils/binutils-2.29.1.tar.bz2
+	tar -xf binutils-2.29.1.tar.bz2
+	cd binutils-2.29.1
+	mkdir build
+	cd build
+	AR=ar AS=as \
+	../configure \
+		--prefix=$TOOLS \
+		--host=$XHOST \
+		--target=$XTARGET \
+		--with-sysroot=$TOOLS \
+		--enable-deterministic-archives \
+		--disable-compressed-debug-sections \
+		--disable-cloog-version-check \
+		--disable-ppl-version-check \
+		--disable-nls \
+		--disable-werror \
+		$MULTILIB
+	make MAKEINFO="true" $MAKEOPTS
+	make MAKEINFO="true" install
+
+	cd $SOURCES
+	wget -c https://github.com/sabotage-linux/kernel-headers/archive/v3.12.6-5.tar.gz
+	tar -xf v3.12.6-5.tar.gz
+	cd kernel-headers-3.12.6-5
+	make ARCH=$XKARCH prefix=/ $MAKEOPTS
+	make ARCH=$XKARCH prefix=/ DESTDIR=$TOOLS install
+
+	cd $SOURCES
+	wget -c http://ftp.gnu.org/gnu/gcc/gcc-7.2.0/gcc-7.2.0.tar.xz
+	wget -c http://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.xz
+	wget -c http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.6.tar.xz
+	wget -c http://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
+	wget -c http://isl.gforge.inria.fr/isl-0.18.tar.xz
+	tar -xf gcc-7.2.0.tar.xz
+	cd gcc-7.2.0
+	tar xf ../mpfr-3.1.6.tar.xz
+	mv mpfr-3.1.6 mpfr
+	tar xf ../gmp-6.1.2.tar.xz
+	mv gmp-6.1.2 gmp
+	tar xf ../mpc-1.0.3.tar.gz
+	mv mpc-1.0.3 mpc
+	tar xf ../isl-0.18.tar.xz
+	mv isl-0.18 isl
+	mkdir build
+	cd build
+	AR=ar \
+	../configure \
+		--prefix=$TOOLS \
+		--build=$XHOST \
+		--host=$XHOST \
+		--target=$XTARGET \
+		--with-sysroot=$TOOLS \
+		--with-newlib \
+		--without-headers \
+		--without-ppl \
+		--without-cloog \
+		--enable-languages=c \
+		--enable-clocale=generic \
+		--disable-gnu-indirect-function \
+		--disable-shared \
+		--disable-threads \
+		--disable-decimal-float \
+		--disable-libgomp \
+		--disable-libssp \
+		--disable-libatomic \
+		--disable-libitm \
+		--disable-libquadmath \
+		--disable-libvtv \
+		--disable-libcilkrts \
+		--disable-libstdcxx \
+		--disable-libmudflap \
+		--disable-libsanitizer \
+		--disable-libmpx \
+		--disable-nls \
+		$MULTILIB \
+		$GCCOPTS
+	make all-gcc all-target-libgcc $MAKEOPTS
+	make install-gcc install-target-libgcc
+
+	cd $SOURCES
+	wget -c http://www.musl-libc.org/releases/musl-1.1.18.tar.gz
+	tar -xf musl-1.1.18.tar.gz
+	cd musl-1.1.18
+	CC="$XTARGET-gcc" CROSS_COMPILE="$XTARGET-" \
+	./configure \
+		--prefix= \
+		--syslibdir=/lib \
+		--enable-debug \
+		--enable-optimize
+	make $MAKEOPTS
+	make DESTDIR=$TOOLS install
+
+	cd $SOURCES
+	rm -rf gcc-7.2.0
+	tar -xf gcc-7.2.0.tar.xz
+	cd gcc-7.2.0
+	tar xf ../mpfr-3.1.6.tar.xz
+	mv mpfr-3.1.6 mpfr
+	tar xf ../gmp-6.1.2.tar.xz
+	mv gmp-6.1.2 gmp
+	tar xf ../mpc-1.0.3.tar.gz
+	mv mpc-1.0.3 mpc
+	tar xf ../isl-0.18.tar.xz
+	mv isl-0.18 isl
+	mkdir build
+	cd build
+	AR=ar \
+	../configure \
+		--prefix=$TOOLS \
+		--build=$XHOST \
+		--host=$XHOST \
+		--target=$XTARGET \
+		--with-sysroot=$TOOLS \
+		--enable-languages=c,c++ \
+		--enable-clocale=generic \
+		--enable-tls \
+		--enable-libstdcxx-time \
+		--enable-checking=release \
+		--enable-fully-dynamic-string \
+		--disable-symvers \
+		--disable-gnu-indirect-function \
+		--disable-libmudflap \
+		--disable-libsanitizer \
+		--disable-libmpx \
+		--disable-nls \
+		--disable-lto-plugin \
+		$MULTILIB \
+		$GCCOPTS
+	make AS_FOR_TARGET="$XTARGET-as" LD_FOR_TARGET="$XTARGET-ld" $MAKEOPTS
+	make install
 }
 
 setup_rootfs() {
@@ -129,11 +275,11 @@ cook_system() {
 	make DESTDIR=$ROOTFS install
 
 	cd $SOURCES
-	wget -c https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.11.tar.xz
-	tar -xf linux-4.14.11.tar.xz
-	cd linux-4.14.11
-	make mrproper
-	make ARCH=$XKARCH CROSS_COMPILE="$XTARGET-" INSTALL_HDR_PATH=$ROOTFS/usr headers_install
+	wget -c https://github.com/sabotage-linux/kernel-headers/archive/v3.12.6-5.tar.gz
+	tar -xf v3.12.6-5.tar.gz
+	cd kernel-headers-3.12.6-5
+	make ARCH=$XKARCH prefix=/usr $MAKEOPTS
+	make ARCH=$XKARCH prefix=/usr DESTDIR=$ROOTFS install
 
 	cd $SOURCES
 	wget -c http://www.musl-libc.org/releases/musl-1.1.18.tar.gz
@@ -149,14 +295,15 @@ cook_system() {
 	make DESTDIR=$ROOTFS install
 
 	cd $SOURCES
-	wget -c https://sortix.org/libz/release/libz-1.2.8.2015.12.26.tar.gz
-	tar -xf libz-1.2.8.2015.12.26.tar.gz
-	cd libz-1.2.8.2015.12.26
+	wget -c http://www.zlib.net/zlib-1.2.11.tar.xz
+	tar -xf zlib-1.2.11.tar.xz
+	cd zlib-1.2.11
 	CROSS_COMPILE="$XTARGET-" \
+	CHOST="$XTARGET" \
 	./configure \
-		$CONFIGURE \
-		--host=$XTARGET
-	make $MAKEOPTS
+		--prefix=/usr \
+		--sharedlibdir=/usr/lib
+	make
 	make DESTDIR=$ROOTFS install
 
 	cd $SOURCES
@@ -170,9 +317,13 @@ cook_system() {
 		$CONFIGURE \
 		--with-sysroot=$ROOTFS \
 		--with-system-zlib \
+		--enable-deterministic-archives \
 		--enable-gold \
 		--enable-ld=default \
+		--enable-lto \
 		--enable-plugins \
+		--disable-compressed-debug-sections \
+		--disable-install-libbfd \
 		--disable-multilib \
 		--disable-nls \
 		--disable-werror \
@@ -221,138 +372,51 @@ cook_system() {
 	make DESTDIR=$ROOTFS install
 	rm -rf $ROOTFS/usr/lib/*.la
 
-#	cd $SOURCES
-#	wget -c http://ftp.gnu.org/gnu/gcc/gcc-7.2.0/gcc-7.2.0.tar.xz
-#	tar -xf gcc-7.2.0.tar.xz
-#	cd gcc-7.2.0
-#	mkdir build
-#	cd build
-#	CROSS_COMPILE="$XTARGET-" \
-#	../configure \
-#		$CONFIGURE \
-#		--with-system-zlib \
-#		--enable-deterministic-archives \
-#		--enable-threads=posix \
-#		--enable-clocale=generic \
-#		--enable-languages=c,c++ \
-#		--enable-libstdcxx-time \
-#		--enable-tls \
-#		--enable-lto \
-#		--enable-libssp \
-#		--disable-bootstrap \
-#		--disable-decimal-float \
-#		--disable-fixed-point \
-#		--disable-gnu-indirect-function \
-#		--disable-libmpx \
-#		--disable-libmudflap \
-#		--disable-libquadmath \
-#		--disable-libstdcxx-pch \
-#		--disable-libsanitizer \
-#		--disable-multilib \
-#		--disable-nls \
-#		--disable-symvers \
-#		--disable-werror \
-#		--host=$XTARGET
-#	make AS_FOR_TARGET="$XTARGET-as" LD_FOR_TARGET="$XTARGET-ld" $MAKEOPTS
-#	make DESTDIR=$ROOTFS install
-#	rm -rf $ROOTFS/usr/lib/*.la
-
 	cd $SOURCES
-	wget -c http://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2
-	tar -xf make-4.2.1.tar.bz2
-	cd make-4.2.1
+	wget -c http://ftp.gnu.org/gnu/gcc/gcc-7.2.0/gcc-7.2.0.tar.xz
+	tar -xf gcc-7.2.0.tar.xz
+	cd gcc-7.2.0
+  # Avoid specific Glibc code
+  ( cd libstdc++-v3/config/os && \
+    mv gnu-linux gnu-linux.orig ; \
+    cp -r generic gnu-linux ; \
+    cp gnu-linux.orig/arm-eabi-extra.ver gnu-linux ; \
+  )
+  ( cd libitm/config && \
+    mv linux/x86 linux/x86_glibc ; \
+    cp -r generic linux/x86 ; \
+  )
+	mkdir build
+	cd build
+	export libat_cv_have_ifunc=no
 	CROSS_COMPILE="$XTARGET-" \
-	./configure \
+	../configure \
 		$CONFIGURE \
-		--without-dmalloc \
-		--without-guile \
+		--with-sysroot=$ROOTFS \
+		--with-system-zlib \
+		--enable-checking=release \
+		--enable-clocale=generic \
+		--enable-__cxa_atexit \
+		--enable-default-pie \
+		--enable-cloog-backend \
+		--enable-languages=c,c++ \
+		--enable-threads=posix \
+		--enable-tls \
+		--disable-bootstrap \
+		--disable-fixed-point \
+		--disable-libmudflap \
+		--disable-libmpx \
+		--disable-libsanitizer \
+		--disable-libssp \
+		--disable-libstdcxx-pch \
+		--disable-multilib \
 		--disable-nls \
+		--disable-symvers \
+		--disable-werror \
 		--host=$XTARGET
-	make $MAKEOPTS
+	make AS_FOR_TARGET="$XTARGET-as" LD_FOR_TARGET="$XTARGET-ld" $MAKEOPTS
 	make DESTDIR=$ROOTFS install
-
-	cd $SOURCES
-	wget -c http://ftp.barfooze.de/pub/sabotage/tarballs/netbsd-curses-0.2.1.tar.xz
-	tar -xf netbsd-curses-0.2.1.tar.xz
-	cd netbsd-curses-0.2.1
-	cat << EOF > config.mak
-CC=$XTARGET-gcc --sysroot=$ROOTFS
-CFLAGS="$XFLAGS -fPIC"
-PREFIX=/usr
-DESTDIR=$ROOTFS
-EOF
-	make $MAKEOPTS
-	make install
-
-	cd $SOURCES
-	wget -c https://github.com/MirBSD/mksh/archive/mksh-R56b.tar.gz
-	tar -xf mksh-R56b.tar.gz
-	cd mksh-R56b
-	sh Build.sh -r
-	install -D -m 755 mksh $ROOTFS/usr/bin/mksh
-	cd $ROOTFS/usr/bin
-	ln -sf mksh sh
-
-	cd $SOURCES
-	wget -c https://www.kernel.org/pub/linux/utils/fs/xfs/xfsprogs/xfsprogs-4.14.0.tar.xz
-	tar -xf xfsprogs-4.14.0.tar.xz
-	cd xfsprogs-4.14.0
-	CROSS_COMPILE="$XTARGET-" \
-	make $MAKEOPTS
-	make DESTDIR=$ROOTFS install
-
-	cd $SOURCES
-	wget -c http://busybox.net/downloads/busybox-1.28.0.tar.bz2
-	tar -xf busybox-1.28.0.tar.bz2
-	cd busybox-1.28.0
-	make ARCH=$XKARCH CROSS_COMPILE="$XTARGET-" defconfig
-	sed -i 's/\(CONFIG_\)\(.*\)\(INETD\)\(.*\)=y/# \1\2\3\4 is not set/g' .config
-	sed -i 's/\(CONFIG_IFPLUGD\)=y/# \1 is not set/' .config
-	sed -i 's/\(CONFIG_FEATURE_WTMP\)=y/# \1 is not set/' .config
-	sed -i 's/\(CONFIG_FEATURE_UTMP\)=y/# \1 is not set/' .config
-	sed -i 's/\(CONFIG_UDPSVD\)=y/# \1 is not set/' .config
-	sed -i 's/\(CONFIG_TCPSVD\)=y/# \1 is not set/' .config
-	make ARCH=$XKARCH CROSS_COMPILE="$XTARGET-" $MAKEOPTS
-	cp busybox $ROOTFS/usr/bin
-	cd $ROOTFS
-	ln -sf usr/bin/busybox init
-	chroot $ROOTFS /usr/bin/busybox --install -s
-
-	cd $SOURCES
-	wget -c https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.6.4.tar.gz
-	tar -xf libressl-2.6.4.tar.gz
-	cd libressl-2.6.4
-	CROSS_COMPILE="$XTARGET-" \
-	./configure \
-		$CONFIGURE \
-		--host=$XTARGET
-	make $MAKEOPTS
-	make DESTDIR=$ROOTFS install
-
-	cd $SOURCES
-	wget -c https://invisible-mirror.net/archives/lynx/tarballs/lynx2.8.8rel.2.tar.gz
-	tar -xf lynx2.8.8rel.2.tar.gz
-	cd lynx2-8-8
-	CROSS_COMPILE="$XTARGET-" \
-	./configure \
-		$CONFIGURE \
-		--with-ssl \
-		--enable-ipv6 \
-		--disable-nls \
-		--host=$XTARGET
-	make $MAKEOPTS
-	make DESTDIR=$ROOTFS install
-
-	cd $SOURCES
-	wget -c https://matt.ucc.asn.au/dropbear/releases/dropbear-2017.75.tar.bz2
-	tar -xf dropbear-2017.75.tar.bz2
-	cd dropbear-2017.75
-	CROSS_COMPILE="$XTARGET-" \
-	./configure \
-		$CONFIGURE \
-		--host=$XTARGET
-	make $MAKEOPTS
-	make DESTDIR=$ROOTFS install
+	rm -rf $ROOTFS/usr/lib/*.la
 }
 
 build_kernel() {
@@ -410,8 +474,8 @@ DEFAULT default
  
 LABEL default
         MENU LABEL JanusLinux
-        LINUX bzImage
-        INITRD rootfs.gz
+	kernel /bzImage
+	append initrd=/rootfs.gz quiet vga=791
 CEOF
 
 	mkdir -p efi/boot
@@ -442,14 +506,15 @@ generate_docker() {
 check_root
 prepare_build
 prepare_cross
-get_toolchain
+prepare_toolchain
+cook_toolchain
 build_prepare
 setup_rootfs
 cook_system
 # strip_filesystem
 # generate_docker
-build_kernel
-generate_iso
+# build_kernel
+# generate_iso
 
 exit 0
 
