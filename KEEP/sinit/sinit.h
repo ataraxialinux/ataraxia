526a991d0c4a7f7b2dcc22455468a83e18d23f63
nee-san <nagisurame@gmail.com>
2019-02-24 06:28:02 +0900

    oh
    Signed-off-by: nee-san <nagisurame@gmail.com>

---
+++ KEEP/sinit/sinit.h
@@ -0,0 +1,3 @@
+static char *const rcinitcmd[] = { "/usr/bin/rc.boot", NULL };
+static char *const rcrebootcmd[] = { "/usr/bin/rc.down", "reboot", NULL };
+static char *const rcpoweroffcmd[] = { "/usr/bin/rc.down", "poweroff", NULL };
