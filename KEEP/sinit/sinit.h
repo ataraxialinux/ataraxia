a8226c20c269772d0db725107a01045c266e1788
Ishimoto Shinobu <nagakamira@gmail.com>
2019-03-04 21:57:36 +0900

    BIG UPDATE
    Signed-off-by: nee-san <nagisurame@gmail.com>

--- KEEP/sinit/sinit.h
+++
@@ -1,3 +0,0 @@
-static char *const rcinitcmd[] = { "/usr/bin/rc.boot", NULL };
-static char *const rcrebootcmd[] = { "/usr/bin/rc.down", "reboot", NULL };
-static char *const rcpoweroffcmd[] = { "/usr/bin/rc.down", "poweroff", NULL };
