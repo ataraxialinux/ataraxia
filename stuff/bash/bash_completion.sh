e56d136d2cce263ffcc72732a29ac7d75e1ec529
Ishimoto Shinobu <nagakamira@gmail.com>
2020-01-18 06:46:34 -0600

    simplify stuff/bash/bash_completion.sh
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- stuff/bash/bash_completion.sh
+++ stuff/bash/bash_completion.sh
@@ -1,20 +1,3 @@
-if [ -f /usr/share/bash-completion/bash_completion ]; then
-	if [ -n "${BASH_VERSION-}" -a -n "${PS1-}" -a -z "${BASH_COMPLETION_VERSINFO-}" ]; then
-		if [ ${BASH_VERSINFO[0]} -gt 4 ] || \
-			[ ${BASH_VERSINFO[0]} -eq 4 -a ${BASH_VERSINFO[1]} -ge 1 ]; then
-			[ -r "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion" ] && \
-				. "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion"
-			if shopt -q progcomp && [ -r /usr/share/bash-completion/bash_completion ]; then
-				. /usr/share/bash-completion/bash_completion
-			fi
-		fi
-	fi
-else
-	if shopt -q progcomp; then
-		for script in /etc/bash_completion.d/* ; do
-			if [ -r $script ] ; then
-				. $script
-			fi
-		done
-	fi
+if [ -n "$BASH_VERSION" ]; then
+	[ -r /usr/share/bash-completion/bash_completion   ] && . /usr/share/bash-completion/bash_completion
 fi
